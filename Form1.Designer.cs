51652c3bfc6ae352ced4700ba8b503b0bf326b29
EagleOmega64 <judahvisagie@gmail.com>
2026-01-16 09:28:24 +0000

    UI tabs
    I added tabs which can now be coded to build our game onto

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,11 +29,76 @@
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
+            timer1 = new System.Windows.Forms.Timer(components);
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            Title = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(304, 130);
+            button1.Name = "button1";
+            button1.Size = new Size(180, 56);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(304, 229);
+            button2.Name = "button2";
+            button2.Size = new Size(180, 56);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(304, 322);
+            button3.Name = "button3";
+            button3.Size = new Size(180, 56);
+            button3.TabIndex = 2;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // Title
+            // 
+            Title.AutoSize = true;
+            Title.BackColor = SystemColors.ActiveCaption;
+            Title.Location = new Point(356, 66);
+            Title.Name = "Title";
+            Title.Size = new Size(84, 15);
+            Title.TabIndex = 3;
+            Title.Text = "THE AUCTION";
+            Title.TextAlign = ContentAlignment.MiddleCenter;
+            Title.Click += label1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
             ClientSize = new Size(800, 450);
+            Controls.Add(Title);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label Title;
     }
 }
