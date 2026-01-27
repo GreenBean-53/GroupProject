70481af4e5df780949dc64dbcce5df6ed71d055e
George Robinson <george.alexander.robinson@outlook.com>
2026-01-27 13:19:17 +0000

    Broken Somewhat

--- Form1.cs
+++ Form1.cs
@@ -35,30 +35,42 @@ namespace GroupProject
 
         }
 
-        private List<String> IssueGenerator()
+        
+    }
+
+    }
+    class Vehicle
+    {
+        private string Name;
+        private int Price;
+        private string Image;
+        private List<String> Issue;
+        private string Positives;
+        private string owner;
+        public List<String> IssueGenerator()
         {
             Console.WriteLine("Issue Selection Triggered");
             List<String> Issues = new List<String>();
-            Issues.Add("Rust");
-            Issues.Add("Engine Knocking");
-            Issues.Add("Broken Shock Absorber");
-            Issues.Add("Misfiring");
-            Issues.Add("Blown Head Gasket");
-            Issues.Add("'Slick' Tyres");
-            Issues.Add("Electrical Issues");
-            Issues.Add("Oil Leak");
-            Issues.Add("Fuel Leak");
-            Issues.Add("Broken Camshaft");
-            Issues.Add("Bullet Holes");
-            Issues.Add("Full of Water");
-            Issues.Add("Misaligned Steering");
-            Issues.Add("Dead Body in the Back");
-            Issues.Add("Suggestion Wheel");
-            Issues.Add("Shattered Windows");
-            Issues.Add("Spontaneous Combustion");
-            Issues.Add("Broken Radiator");
-            Issues.Add("Broken Radio");
-            Issues.Add("Filled with Asbestos");
+            Issues.Add("Rust", -3000);
+            Issues.Add("Engine Knocking", -500);
+            Issues.Add("Broken Shock Absorber", -100);
+            Issues.Add("Misfiring", -600);
+            Issues.Add("Blown Head Gasket", -200);
+            Issues.Add("'Slick' Tyres", -400);
+            Issues.Add("Electrical Issues", -50);
+            Issues.Add("Oil Leak", -70);
+            Issues.Add("Fuel Leak", -400);
+            Issues.Add("Broken Camshaft", -300);
+            Issues.Add("Bullet Holes", -900);
+            Issues.Add("Full of Water", -200);
+            Issues.Add("Misaligned Steering", -60);
+            Issues.Add("Dead Body in the Back", -4000);
+            Issues.Add("Suggestion Wheel", -2000);
+            Issues.Add("Shattered Windows", -600);
+            Issues.Add("Spontaneous Combustion", -10000);
+            Issues.Add("Broken Radiator", -600);
+            Issues.Add("Broken Radio", -30);
+            Issues.Add("Filled with Asbestos", -6000);
             List<String> GeneratedIssue = new List<String>();
             for (int i = 0; i < 3; i++)
             {
@@ -69,80 +81,69 @@ namespace GroupProject
             }
             return GeneratedIssue;
         }
-        private List<String> VehicleGenerator()
+        public List<String> VehicleGenerator()
         {
             Console.WriteLine("Vehicle Selection Triggered");
             List<String> Vehicles = new List<String>();
-            Vehicles.Add("Volvo 850r");
-            Vehicles.Add("Subaru Legacy");
-            Vehicles.Add("Subaru Impreza");
-            Vehicles.Add("Chevrolet Bel Air");
-            Vehicles.Add("Chevrolet Silverado");
-            Vehicles.Add("Suzuki Jimney");
-            Vehicles.Add("Bismarck");
-            Vehicles.Add("Yamato");
-            Vehicles.Add("MiG-15");
-            Vehicles.Add("Fairey Swordfish");
-            Vehicles.Add("Churchill Gun Carrier");
-            Vehicles.Add("T34");
-            Vehicles.Add("Toyota Land Cruiser");
-            Vehicles.Add("B29 Enola Gay");
-            Vehicles.Add("Reliant Robin");
-            Vehicles.Add("RedBull Soapbox");
-            Vehicles.Add("RedBull Mini");
-            Vehicles.Add("Carthaginian War Elephant");
-            Vehicles.Add("Mustang Mach 1");
-            Vehicles.Add("Williams FW14B");
+            Vehicles.Add(("Volvo 850r", "Volvo.png"));
+            Vehicles.Add("Subaru Legacy", "Legacy.png");
+            Vehicles.Add("Subaru Impreza", "Impreza.png");
+            Vehicles.Add("Chevrolet Bel Air", "BelAir.png");
+            Vehicles.Add("Chevrolet Silverado", "Silverado.png");
+            Vehicles.Add("Suzuki Jimney", "Jimny.png");
+            Vehicles.Add("Bismarck", "");
+            Vehicles.Add("Yamato", "");
+            Vehicles.Add("MiG-15", "");
+            Vehicles.Add("Fairey Swordfish", "");
+            Vehicles.Add("Churchill Gun Carrier", "");
+            Vehicles.Add("T34", "");
+            Vehicles.Add("Toyota Land Cruiser", "LandCruiser.png");
+            Vehicles.Add("B29 Enola Gay", "");
+            Vehicles.Add("Reliant Robin", "ReliantRobin.png");
+            Vehicles.Add("RedBull Soapbox", "SoapBox.png");
+            Vehicles.Add("RedBull Mini", "RBmini.png");
+            Vehicles.Add("Carthaginian War Elephant", "WarElephant.png");
+            Vehicles.Add("Mustang Mach 1", "Mustang.png");
+            Vehicles.Add("Williams FW14B", "WilliamsFW14B.png");
 
-            List<String> GeneratedVehicle = new List<String>();
             Random num1 = new Random();
             int int1 = num1.Next(20);
             Console.WriteLine(int1);
             //number used to get the other things
-
-            List<String> Images = new List<String>();
-            Images.Add("Volvo.png");
-            Images.Add("Legacy.png");
-            Images.Add("Impreza.png");
-            Images.Add("BelAir.png");
-            Images.Add("Silverado.png");
-            Images.Add("Jimny.png");
-            Images.Add("");
-            Images.Add("");
-            Images.Add("");
-            Images.Add("");
-            Images.Add("");
-            Images.Add("");
-            Images.Add("LandCruiser.png");
-            Images.Add("");
-            Images.Add("ReliantRobin.png");
-            Images.Add("SoapBox.png");
-            Images.Add("RBmini.png");
-            Images.Add("WarElephant.png");
-            Images.Add("Mustang.png");
-            Images.Add("WilliamsFW14B.png");
-
-            return GeneratedVehicle;
+            return Vehicles(int1);
         }
 
-
-    }
-
-    }
-    class Vehicle
-    {
-        private string Name;
-        private int Price;
-        private string Image;
-        private string Issue;
-        private string Positives;
-        private string owner;
+        public List<String> PositiveGenerator()
+        {
+            Console.WriteLine("Positive Generation Triggered");
+            List<String> Positives = new List<String>();
+            Positives.Add("Trustworthy Owner", 3000);
+            Positives.Add("Valid MOT", 500);
+            Positives.Add("Rare Model", 6000);
+            Positives.Add("Filled With Gold", 2000);
+            Positives.Add("Spare Wheel", 200);
+            List<String> GeneratedPositive = new List<String>();
+            Random num1 = new Random();
+            int int1 = num1.Next(50);
+            if (int1 <= 4)
+            {
+                GeneratedPositive.Add(Positives(int1));
+            }
+            Console.WriteLine(GeneratedPositive);
+            return GeneratedPositive;
+        }
         
+
         public Vehicle(String pName, int pPrice, string pImage)
         {
-            Name = pName;
+            Name = VehicleGenerator();
             Price = pPrice;
             Image = pImage;
-            owner = "No Owner";
+            Issue = IssueGenerator();
+            Positives = PositiveGenerator();
+
+            
         }
+
+        
     }
