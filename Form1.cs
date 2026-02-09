7775dada5aada3a27c60f5f5a845e7cb4cb9ea55
George Robinson <george.alexander.robinson@outlook.com>
2026-02-09 08:46:27 +0000

    Image works somewhat

--- Form1.cs
+++ Form1.cs
@@ -95,6 +95,6 @@ public partial class Game : Form
         startButton.Enabled = false;
         startButton.Visible = false;
         
-        vehcilePictureBox.Image = Image.FromFile(@"https://github.com/GreenBean-53/GroupProject/tree/master/Images/");
+        vehcilePictureBox.Image = Image.FromFile(@"C:\Users\georg\OneDrive\Documents\GitHub\GroupProject1\GroupProject\Images\Impreza.png");
     }
 }
